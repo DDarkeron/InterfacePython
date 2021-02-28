ecf02a026f596d3b8b28de835ce55434465bcfe0
DDarkeron <31534102+DDarkeron@users.noreply.github.com>
2021-02-28 16:31:54 +0300

    Add files via upload

--- clientTCP.py
+++ clientTCP.py
@@ -1,52 +1,57 @@
-import socket
 import json
-#from ipaddress import ip_address
 import ipaddress
+import time
+from pyroute2 import IPRoute
+import socket, struct, fcntl
 class Client:
-    def getFile(self):#Getting temporary settings for new device
-        sock = socket.socket()
-        sock.connect(('localhost', 9090))
-        #sock.send('hello, world!')
-        f = open('configs.json',"w")
-        data = sock.recv(1024)
-        while(data):
-            f.write(data)
-            data = sock.recv(1024)
-        sock.close()
-        f.close()
+    """
+    This class represents device that is manipulated by main machine
+    """
+
+    def __init__(self, ip, gate, mask, number):
+        self.ip = ip
+        self.gate = gate
+        self.mask = mask
+        self.name = 'device' + str(number)
+        now = time.localtime()
+        self.time = time.strftime("%H:%M:%S", now)
+
+    def write_config(self):
+        """Writes down starting config"""
+        obj = [
+            [self.ip,
+             self.gate,
+             self.mask,
+             self.name,
+             self.time]
+        ]
+        with open('config.json', 'wt') as jsonfile:
+            json.dump(obj, jsonfile)
+
+    def set_start_configuration(self):
+        """Setting default internet configuration"""
+        with open('config.json', 'rt') as jsonfile:
+            configuration = json.read()
+        configuration_data = json.loads(configuration)
+        ip = IPRoute()
+        index = ip.link_lookup(ifname='em1')[0]
+        ip.addr('add', index, address=self.ip, mask=24)
+        ip.close()
+
+    def send_respond(self):
+        """Sending respond to the server to get new address"""
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect(('192.168.120.3', 9090))
+
+    def set_new_configuration(self):
+        """Setting new configuration given by the server """
 
 
-    def setTempConf(self):#Setting temporary setting
-        with open('config.json','r') as fjson:
-            conf = fjson.read()#read file
-        obj = json.loads(conf)#parse data
-        #print(obj)
-        print(type(obj))
-        print(obj.get('ip'))
-        #print(obj.ip)
-        #print(obj['configs'][self.ip])
-        ipaddress.IPv4Address(obj.get('ip'))
-
-
-    def setNewConf(self):#Setting new settings for device
-        print('k')
-
-    def sendNewConf(self):#Sending new configuration to server
-        sock = socket.socket()
-        sock.connect(('localhost',9090))
-        #sock.listen(1)
-        #conn, addr = sock.accept()
-        f = open("newConfigs.json","r")
-        l =f.read(1024)
-        while (l):
-            conn.send(l)
-            l =f.read(1024)
-        conn.close()
-        f.close()
-
-if __name__=="__main__":
-   # self.getFile()
-    clt = Client()
-    clt.setTempConf()
-    #self.setNewConf()
-    #self.sendNewConf()
+if __name__ == "__main__":
+    clt = Client('169.254.3.3', '169.254.3.3', '255.255.255.0', 0)
+    clt.write_config()
+    clt.set_start_configuration()
+    while True:
+        #break
+        clt.send_respond()
+        clt.set_new_configuration()
--- serverTCP.py
+++ serverTCP.py
@@ -1,90 +1,120 @@
-#import socket
-#sock = socket.socket()
-#sock.bind(('',9090))
-#sock.listen(1)
-#conn, addr = sock.accept()
-#while True:
-#    data = conn.recv(1024)
-#    if not data:
-#        break
-#    conn.send(data.upper())
-#
-#conn.close()
 import socket
 import json
-class Con:
-
-    def writeConfig(self):
-        #Creating json file
-        obj ={}
-       # obj['configs'] = []
-        obj.update({
-            'ip': self.ip,
-            'gate': self.gate,
-            'mask': self.mask,
-            'filename': self.filename
-        })
-        with open('config.json','w') as fjson:
-            json.dump(obj, fjson)
+import time
 
 
-    def __init__(self):
-        self.ip = '169.254.3.3'
-        self.gate = '169.254.3.1'
-        self.mask = '255.255.255.0'
-        self.filename = 'config.json'
-
-    def sendFileToClient(self):#Sending configs to client
-        sock = socket.socket()
-        sock.bind(('',9090))
-        sock.listen(1)
-        conn, addr = sock.accept()
-        f = open("configs.json","r")
-        l =f.read(1024)
-        while (l):
-            conn.send(l)
-            l =f.read(1024)
-        conn.close()
-        f.close()
-
-    def getNewDevice(self):#Add new device to list
-        #data= {}
-        #data{}['config'].append({  })
-        sock = socket.socket()
-        sock.bind(('',9090))
-        sock.listen(1)
-        conn, addr = sock.accept()
-        fjson = open("newConfig.json","w")
-        data = conn.recv(1024)
-        while (data):
-            fjson.write(data)
-            data = conn.recv(1024)
-        conn.close()
-        fjson.close()
-        with open('newConfig.json','r') as fjson:
-            conf = fson.read()#read file
-        obj = json.load(conf)#parse file
-        return obj
-
-    def checkUpdate(self):#Checking if file has been changed
-        print('k')
-
-
-if __name__=="__main__":
-    #createConfig()
+class Server:
     """
-    socket.setdefaulttimeout(5)
-    chest ={}#list of devices
-    con = Con()
-    con.writeConfig()
-    con.sendFileToClient()
-    chest['devices'].append(con.getNewDevice())
-    while True:
-        self.sendFileToClient()
-        chest['devices'].append(self.getNewDevice())
-        """
-    con = Con()
-    con.writeConfig()
+    Represents main controlling device
+    Adds and stores all secondhand devices, trying to reach the main machine(this machine)
+    """
 
- 
+    def __init__(self, ip, gate, mask, number):
+        self.ip = ip
+        self.gate = gate
+        self.mask = mask
+        self.name = 'device' + str(number)
+        now = time.localtime()
+        self.time = time.strftime("%H:%M:%S", now)
+
+    def write_config(self):
+        """Writes down starting config"""
+        device_data = [
+            [self.ip,
+             self.gate,
+             self.mask,
+             self.name,
+             self.time]
+        ]
+        with open('config.json', 'wt') as jsonfile:
+            json.dump(device_data, jsonfile)
+
+    def getNewDevice(self):
+        """Adds new device to list and immediately sends new configuration to client """
+        with open('config.json', 'rt') as jsonfile:
+            configuration = jsonfile.read()
+        configuration_data = json.loads(configuration)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            try:
+                sock.settimeout(2)
+                sock.bind(('', 9090))
+                sock.listen(1)
+                conn, addr = sock.accept()
+            except socket.timeout:
+                return False
+            sock.settimeout(None)
+            with conn:
+                conn.send(configuration)
+        return configuration_data
+
+    def check_update(self, my_dict, size, my_list):
+        """Checking if file has been changed or device has been removed"""
+        port = '8080'
+        removed_devices = []
+        if type(my_dict) is list:
+            for i in range(0, size):
+                host = my_dict[i][0]
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                    try:
+                        sock.settimeout(10)
+                        sock.connect((host, port))
+                        try:
+                            while data:
+                                data = sock.recv(1024)
+                        except OSError as msg:
+                            print(msg)
+                            continue
+                        client_device_data = json.loads(data)
+                        if client_device_data['time'] is not my_dict[i]['time']:
+                            with sock:
+                                sock.send(str(my_dict))
+                    except socket.timeout:
+                        removed_devices.update(i)
+                        continue
+        else:
+            print('Object is not a dictionary')
+            return my_dict
+        for k in removed_devices:
+            my_dict[k].pop()
+            my_list[k][1] = False
+        return my_dict, my_list
+
+    def write_all_devices(self, my_dict):
+        """ Writes down all devices that are connected at the moment"""
+        with open("devices.json", 'wt') as jsonfile:
+            json.dump(my_dict, jsonfile)
+
+    def set_free_address(self, my_list):
+        """Sets address for the next device"""
+        with open('config.json', 'rt') as jsonfile:
+            configuration = jsonfile.read()
+        configuration_data = json.loads(configuration)
+        for ii in range(0, 250):
+            if my_list[ii][1] is False:
+                configuration_data[0][0] = '192.168.120.' + str(ii+5)
+                configuration_data[0][3] = 'device' + str(my_list[ii][0])
+                my_list[ii][1] = True
+        with open('config.json', 'wt') as jsonfile:
+            json.dump(configuration_data, jsonfile)
+
+
+if '__main__' == __name__:
+    free_addresses = list()
+    for n in range(0, 250):
+        free_addresses.append([n+5, False])
+    srv = Server('192.168.120.5', '192.168.120.1', '255.255.255.0', 0)
+    devices = []
+    srv.write_config()
+    while True:
+        #break
+        srv.set_free_address(free_addresses)
+        new_device = srv.getNewDevice()
+        if new_device is not False:
+            devices.append(new_device)
+            srv.write_all_devices(devices)
+        else:
+            pass
+        devices, free_addresses = srv.check_update(devices, int(len(devices)/5), free_addresses )
+        time.sleep(5)
+
 
